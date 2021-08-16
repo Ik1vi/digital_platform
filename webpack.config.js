fc4ab6388a1659c14e48a0cd5a6434c8fea42c16
Irene Veselova <ik1vikuz@gmail.com>
2021-08-16 12:40:02 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        // {
+        //     test: /\.html$/,
+        //     use: "html-loader"
+        // },
+        // {
+        //     test: /\.css$/,
+        //     use: ["style-loader", "css-loader"],
+        // },
+        {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        // {
+        //     test: /\.m?js$/,
+        //     exclude: /(node_modules|bower_components)/,
+        //     use: {
+        //         loader: 'babel-loader',
+        //         options: {
+        //             presets: ['@babel/preset-env'],
+        //         }
+        //     },
+        // },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/,
+            type: 'asset/resource',
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            type: 'asset/resource',
+        }
+    ]
+},
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "./favicon.png",
+      template: path.resolve(__dirname, "index.html")
+    }),
+  ],
+};
